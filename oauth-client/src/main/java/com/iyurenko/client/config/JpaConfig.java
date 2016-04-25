5218732b727336e4eab1fd7c57bc099932c73586
iyurenko <iyurenko@griddynamics.com>
2016-04-25 18:03:48 +0300

    Code refactoring. (Usage the spring-boot-starter-data-jpa instead the entity manager manually  configuration)

--- oauth-client/src/main/java/com/iyurenko/client/config/JpaConfig.java
+++ oauth-client/src/main/java/com/iyurenko/client/config/JpaConfig.java
@@ -1,26 +1,9 @@
 package com.iyurenko.client.config;
 
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.jdbc.datasource.init.DataSourceInitializer;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.jpa.JpaDialect;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.Database;
-import org.springframework.orm.jpa.vendor.HibernateJpaDialect;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
-import javax.persistence.EntityManagerFactory;
-import java.util.Properties;
 
 /**
  * @author iyurenko
@@ -30,74 +13,4 @@ import java.util.Properties;
 @EnableTransactionManagement(proxyTargetClass = true)
 @EnableJpaRepositories("com.iyurenko.client.dao.repository")
 public class JpaConfig {
-
-
-    @Value("${spring.datasource.url}")
-    private String dbUrl;
-
-    @Value("${spring.datasource.username}")
-    private String username;
-
-    @Value("${spring.datasource.password}")
-    private String password;
-
-    @Value("${spring.datasource.driver-class-name}")
-    private String driver;
-
-
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(driver);
-        dataSource.setUrl(dbUrl);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
-        return dataSource;
-    }
-
-    @Bean
-    public EntityManagerFactory entityManagerFactory() {
-        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        vendorAdapter.setGenerateDdl(true);
-        vendorAdapter.setShowSql(false);
-        vendorAdapter.setDatabasePlatform("org.hibernate.dialect.MySQL5InnoDBDialect");
-        vendorAdapter.setDatabase(Database.MYSQL);
-
-        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
-        factory.setJpaVendorAdapter(vendorAdapter);
-        factory.setPackagesToScan("com.iyurenko.client.dao.domain");
-        factory.setDataSource(dataSource());
-
-        Properties properties = new Properties();
-//        properties.setProperty("hibernate.cache.region.factory_class", "org.hibernate.cache.ehcache.EhCacheRegionFactory");
-//        properties.setProperty("hibernate.cache.use_query_cache", "true");
-//        properties.setProperty("hibernate.generate_statistics", "true");
-//        factory.setJpaProperties(properties);
-        factory.afterPropertiesSet();
-
-        return factory.getObject();
-    }
-
-
-
-
-    @Bean
-    @Autowired
-    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-        JpaTransactionManager txManager = new JpaTransactionManager();
-        JpaDialect jpaDialect = new HibernateJpaDialect();
-        txManager.setEntityManagerFactory(entityManagerFactory);
-        txManager.setJpaDialect(jpaDialect);
-        return txManager;
-    }
-
-//    @Bean
-//    @Autowired
-//    public HibernateTransactionManager transactionManager(SessionFactory s) {
-//        HibernateTransactionManager txManager = new HibernateTransactionManager();
-//        txManager.setSessionFactory(s);
-//        return txManager;
-//    }
-
-
 }
